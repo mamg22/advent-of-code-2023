2001529f27757bb7d0473290ca03c3c80ee079cf
mamg22 <45301823+mamg22@users.noreply.github.com>
2023-12-10 18:26:19 -0400

    Typing

--- aoc/common.py
+++ aoc/common.py
@@ -1,15 +1,15 @@
 class Grid:
-    def __init__(self, width, height, default_value=None):
+    def __init__(self, width: int, height:int, default_value=None):
         self.width = width
         self.height = height
         self.content = [[default_value] * width for _ in range(height)]
     
-    def get(self, x, y):
+    def get(self, x: int, y: int):
         if x < 0 or x > self.width or y < 0 or y > self.height:
             raise IndexError(f"({x}, {y}) is out of grid area")
         return self.content[y][x]
 
-    def set(self, x, y, value):
+    def set(self, x: int, y: int, value):
         if x < 0 or x > self.width or y < 0 or y > self.height:
             raise IndexError(f"({x}, {y}) is out of grid area")
         self.content[y][x] = value
