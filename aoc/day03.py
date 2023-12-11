6c0f6b53b4983a3662991a83bb100236508e6efe
mamg22 <45301823+mamg22@users.noreply.github.com>
2023-12-11 00:55:54 -0400

    Put back the Grid in day 3

--- aoc/day03.py
+++ aoc/day03.py
@@ -3,8 +3,21 @@ import string
 from functools import reduce
 from dataclasses import dataclass
 
-from common import Grid
+class Grid:
+    def __init__(self, width: int, height:int, default_value=None):
+        self.width = width
+        self.height = height
+        self.content = [[default_value] * width for _ in range(height)]
+    
+    def get(self, x: int, y: int):
+        if x < 0 or x > self.width or y < 0 or y > self.height:
+            raise IndexError(f"({x}, {y}) is out of grid area")
+        return self.content[y][x]
 
+    def set(self, x: int, y: int, value):
+        if x < 0 or x > self.width or y < 0 or y > self.height:
+            raise IndexError(f"({x}, {y}) is out of grid area")
+        self.content[y][x] = value
 @dataclass
 class Element:
     def __init__(self, x, y):
